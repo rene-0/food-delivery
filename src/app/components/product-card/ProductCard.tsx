98cc473213fdf20992eaaa517d4ad77ffffa3061
rene-0 <rene.colombaro@hotmail.com>
2025-03-23 16:33:11 -0300

    feat: add css

--- src/app/components/product-card/ProductCard.tsx
+++ src/app/components/product-card/ProductCard.tsx
@@ -4,6 +4,7 @@ import { ComponentProps } from "react"
 import { twMerge } from "tailwind-merge"
 import { Button } from "../button/Button"
 import { ProductStars } from "./components/product-stars/ProductStars"
+import "./product-card.css"
 
 type ProductCardProps = ComponentProps<"div"> & {
   price: number
@@ -15,18 +16,17 @@ type ProductCardProps = ComponentProps<"div"> & {
 export function ProductCard({ productName, price, starsPercentageRating, imageUrl, ...rest }: ProductCardProps) {
   return (
     <div
-      style={{ width: 250 }}
-      className={twMerge("flex flex-col items-center shrink-0 p-5 bg-primary-200 shadow-lg rounded cursor-pointer", rest.className)}
+      className={twMerge("product-card text-xl lg:text-3xl flex flex-col items-center shrink-0 p-5 bg-primary-200 shadow-lg rounded", rest.className)}
     >
       <Image src={imageUrl} alt="burger" width={300} height={300} />
-      <p className="text-3xl font-black text-accent-950">{productName}</p>
+      <p className="font-black text-accent-950">{productName}</p>
       <ProductStars starsPercentageRating={starsPercentageRating} />
-      <p className="text-3xl font-black text-secondary-950/50">{price} R$</p>
+      <p className="font-black text-secondary-950/50">{price} R$</p>
       <div className="flex items-center mt-2">
-        <Button className="text-secondary-500 bg-primary-200 hover:text-white border-accent-950 border-2 hover:bg-accent-950 h-10 w-10 p-0 rounded-full mr-5 ease-in-out duration-300">
+        <Button className="text-secondary-500 bg-primary-200 hover:text-white border-accent-950 border-2 hover:bg-accent-950 h-10 w-10 p-0 rounded-full mr-1 lg:mr-5 ease-in-out duration-300">
           <PlusOutlined />
         </Button>
-        <Button className="text-lg text-white font-semibold mr-0 py-2 px-8 rounded-full cursor-pointer bg-accent-950 drop-shadow-md">Comprar</Button>
+        <Button className="px-4 lg:px-8">Comprar</Button>
       </div>
     </div>
   )
--- src/app/contato/components/location-map/LocationMap.tsx
+++ src/app/contato/components/location-map/LocationMap.tsx
@@ -4,13 +4,16 @@ import { MapContainer } from "react-leaflet/MapContainer"
 import { Marker } from "react-leaflet/Marker"
 import { Popup } from "react-leaflet/Popup"
 import { TileLayer } from "react-leaflet/TileLayer"
+import "./location-map.css"
+
 const myIcon = new Icon({
   iconUrl: "/marker2.png",
   iconSize: [25, 41],
 })
+
 export function LocationMap() {
   return (
-    <MapContainer center={[-22.0737, -48.737]} zoom={15} scrollWheelZoom={true} style={{ height: "100%", width: "100%" }}>
+    <MapContainer className="location-map" center={[-22.0737, -48.737]} zoom={15} scrollWheelZoom={true}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
