620bbc93ca3245e59e0134a9fc6a399ddb265ff2
rene-0 <rene.colombaro@hotmail.com>
2025-09-19 14:57:49 -0300

    fix: ensure stars colors is correct

--- src/app/components/product-card/components/product-stars/ProductStars.tsx
+++ src/app/components/product-card/components/product-stars/ProductStars.tsx
@@ -9,18 +9,18 @@ export function ProductStars({ starsPercentageRating }: ProductStarsProps) {
   return (
     <div className="product-stars relative">
       <div className="absolute flex z-10">
-        <StarFilled className="text-accent-950/40 text-xl" />
-        <StarFilled className="text-accent-950/40 text-xl" />
-        <StarFilled className="text-accent-950/40 text-xl" />
-        <StarFilled className="text-accent-950/40 text-xl" />
-        <StarFilled className="text-accent-950/40 text-xl" />
+        <StarFilled className="text-accent-950/40! text-xl" />
+        <StarFilled className="text-accent-950/40! text-xl" />
+        <StarFilled className="text-accent-950/40! text-xl" />
+        <StarFilled className="text-accent-950/40! text-xl" />
+        <StarFilled className="text-accent-950/40! text-xl" />
       </div>
       <div style={{ width: `${starsPercentageRating}%` }} className="flex overflow-hidden absolute z-20">
-        <StarFilled className="text-accent-950 text-xl" />
-        <StarFilled className="text-accent-950 text-xl" />
-        <StarFilled className="text-accent-950 text-xl" />
-        <StarFilled className="text-accent-950 text-xl" />
-        <StarFilled className="text-accent-950 text-xl" />
+        <StarFilled className="text-accent-950! text-xl" />
+        <StarFilled className="text-accent-950! text-xl" />
+        <StarFilled className="text-accent-950! text-xl" />
+        <StarFilled className="text-accent-950! text-xl" />
+        <StarFilled className="text-accent-950! text-xl" />
       </div>
     </div>
   )
