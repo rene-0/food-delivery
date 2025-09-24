1abdef1f4fea522c7f7fea1b12bbfdc84f8e22d0
rene-0 <rene.colombaro@hotmail.com>
2025-09-24 18:36:52 -0300

    feat: enhance CheckoutItemRoot component with props forwarding and class merging

--- src/app/components/checkout-item-composition/CheckoutItemRoot.tsx
+++ src/app/components/checkout-item-composition/CheckoutItemRoot.tsx
@@ -1,21 +1,22 @@
 "use client"
-import { createContext, useState } from "react"
+import { createContext, DetailedHTMLProps, HTMLAttributes, useState } from "react"
+import { twMerge } from "tailwind-merge"
 
 export const CheckoutItemContext = createContext(false)
 
 type CheckoutItemRootProps = {
 	children: React.ReactNode
-}
+} & DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
 
-export function CheckoutItemRoot({ children }: CheckoutItemRootProps) {
+export function CheckoutItemRoot({ children, ...rest }: CheckoutItemRootProps) {
 	const [isSelected, setIsSelected] = useState(false)
-	console.log('CheckoutItemRoot', isSelected)
 
 	return (
 		<CheckoutItemContext.Provider value={isSelected}>
 			<div
+				{...rest}
 				onClick={() => setIsSelected((oldIsSelected) => !oldIsSelected)}
-				className="checkout-item p-2 mb-3 last:mb-1 hover:bg-primary-50 hover:shadow transition ease-in-out duration-300 cursor-pointer"
+				className={twMerge("checkout-item p-2 mb-3 last:mb-1 hover:bg-primary-50 hover:shadow transition ease-in-out duration-300 cursor-pointer", rest.className)}
 			>
 				{children}
 			</div>
