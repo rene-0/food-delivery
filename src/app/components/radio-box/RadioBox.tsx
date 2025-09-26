89f510048d45d65dd2744a4f986157036665befd
rene-0 <rene.colombaro@hotmail.com>
2025-09-26 11:47:46 -0300

    feat: enhance radio box to trigger when dot is clicked

--- src/app/components/radio-box/RadioBox.tsx
+++ src/app/components/radio-box/RadioBox.tsx
@@ -12,7 +12,7 @@ export function RadioBox({ children, name, id, ...rest }: RadioBoxProps) {
   return (
     <div aria-disabled={rest.disabled} className="radio-box flex mr-2 mb-2 cursor-pointer">
       <input {...rest} type="radio" name={name} id={id} />
-      <div className="marker flex items-center justify-center rounded-full bg-accent-950/95 border-secondary-950/70" />
+      <label htmlFor={id} className="marker flex items-center justify-center rounded-full bg-accent-950/95 border-secondary-950/70" />
       <label className="text-secondary-950/70 font-bold text-lg ml-2" htmlFor={id}>
         {children}
       </label>
